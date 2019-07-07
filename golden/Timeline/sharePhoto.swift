d1b372d6d0c7c7434e240ae165c73cc08d2637d4
kazuyasasakura <kazuya.sasakura@gmial.com>
2019-07-07 18:52:05 +0900

    icon add....

---
+++ golden/Timeline/sharePhoto.swift
@@ -0,0 +1,30 @@
+//
+//  sharePhoto.swift
+//  golden
+//
+//  Created by 笹倉一也 on 2019/07/07.
+//  Copyright © 2019 笹倉一也. All rights reserved.
+//
+
+import UIKit
+
+class sharePhoto: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
